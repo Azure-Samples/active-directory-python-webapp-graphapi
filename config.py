08051d48745c05fa22ecfd73f126a34d1da4002a
Navya Canumalla <nacanuma@microsoft.com>
2020-01-29 13:04:05 -0800

    Archiving the sample

--- config.py
+++
@@ -1,8 +0,0 @@
-RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
-TENANT = "Your tenant"  # Enter tenant name, e.g. contoso.onmicrosoft.com
-AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
-CLIENT_ID = "Your client id "  # copy the Application ID of your app from your Azure portal
-CLIENT_SECRET = "Your client secret"  # copy the value of key you generated when setting up the application
-
-# These settings are for the Microsoft Graph API Call
-API_VERSION = 'v1.0'
